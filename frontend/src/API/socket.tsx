b1e5ef041b9436cd4282fa5d3c9a00c3424895e4
am.mussy <aydoganwork@gmail.com>
2023-01-28 23:06:12 +0300

    fix ssl server

--- frontend/src/API/socket.tsx
+++ frontend/src/API/socket.tsx
@@ -3,6 +3,7 @@ import { io } from "socket.io-client";
 
 export const socket = io("https://94.228.118.237:4000/", {
   transports: ["websocket"],
+  secure: true,
 });
 export const CLEAR_VOTES_VALUE = "CLEAR_VOTES_VALUE";
 export const JOIN_TO_ROOM = "JOIN_TO_ROOM";
