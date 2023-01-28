d89214907be02365b06988223f23920ea131940b
am.mussy <aydoganwork@gmail.com>
2023-01-28 14:51:09 +0300

    fix https

--- frontend/src/API/socket.js
+++ frontend/src/API/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://www.planning-time:4000", {
+export const socket = io("https://127.0.0.1:4000", {
   transports: ["websocket"],
   secure: true,
 });
