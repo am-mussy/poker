986fa0f3a480b765bbb9386fc60d67f7a4debaea
am.mussy <aydoganwork@gmail.com>
2023-02-05 23:40:57 +0300

    fix code style & remove unused files

--- frontend/src/features/ClearVotesValue/ClearVotesValue.tsx
+++ frontend/src/features/ClearVotesValue/ClearVotesValue.tsx
@@ -1,25 +1,22 @@
 import NeuButton from "../../shared/button/NeuButton";
 import React from "react";
-import {roomSlice} from "../../store/reducers/RoomSlice";
-import {useAppDispatch} from "../../hooks/hooks";
-
+import { roomSlice } from "../../store/reducers/RoomSlice";
+import { useAppDispatch } from "../../hooks/hooks";
 
 const ClearVotesValue = () => {
+  const dispatch = useAppDispatch();
+  const { clearVotesValueAction } = roomSlice.actions;
+  const clearVotesValue = () => dispatch(clearVotesValueAction());
 
-    const dispatch = useAppDispatch();
-    const { clearVotesValueAction } = roomSlice.actions;
-    const clearVotesValue = () => dispatch(clearVotesValueAction())
-
-
-    return (
-        <div>
-            <NeuButton
-                className={"show-btn"}
-                onClick={clearVotesValue}
-                name={`${"Очистить"}`}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <NeuButton
+        className={"show-btn"}
+        onClick={clearVotesValue}
+        name={`${"Очистить"}`}
+      />
+    </div>
+  );
 };
 
 export default ClearVotesValue;
